2d51b6fdfa95c2c4fed92a0ee66eeb38a59cf876
Sandeep650 <61959838+Sandeep650@users.noreply.github.com>
2020-03-12 11:18:43 +0530

    Update week1problem1.java

--- week1problem1.java
+++ week1problem1.java
@@ -1,1 +1,24 @@
-
+import java.lang.*;
+import java.ytil.*;
+class one
+{
+  public static void main(String args[])
+  {
+    Scanner sc=new Scanner sc.nextInt(System.in);
+    int n=sc.nextInt();
+    int a[]=new int[n];
+    for(int i=0;i<n;i++)
+      a[i]=sc.nextInt();
+    int q=sc.nextInt();
+    for(int i=0;i<q;i++)
+    {
+      int s=0;
+      int x=sc.nextInt();
+      int y=sc.nextInt();
+      for(int j=x;j<=y;j++)
+        s^=a[j];
+      System.out.print(s+" ");
+    }
+    
+  }
+}
